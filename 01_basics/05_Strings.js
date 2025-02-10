1772ec59ad53aa2575a3940a449c6fc8daead057
Vishal Tomar <90099169+vishaltomar004@users.noreply.github.com>
2025-02-10 12:16:05 +0000

    maths and fun completed

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,8 @@
+const name ="bhoolo";
+const count=90;
+//  console.log(name + count + "jo bhi hai");
+ 
+
+console.log(name.charAt(2));
+console.log(name.indexOf('o'));
+
---
+++ 01_basics/06_nums_ans_maths.js
@@ -0,0 +1,19 @@
+// const score =400
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+
+// const a = 34.56
+// console.log(a.toPrecision(2));
+
+
+
+console.log(Math);
+console.log(Math.round(4.56));
+console.log(Math.ceil(4.56));
+
+
