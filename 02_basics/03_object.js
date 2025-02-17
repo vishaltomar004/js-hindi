e48b98ac7f9aef31dcecc9c33776c48bc2c7e430
Vishal Tomar <90099169+vishaltomar004@users.noreply.github.com>
2025-02-17 11:29:53 +0000

    Objectas

---
+++ 02_basics/03_object.js
@@ -0,0 +1,8 @@
+
+const JsUser = {
+    name : "Vishal",
+    age:18,
+    location :"jaipur",
+    email : "@ham.com",
+    "full name" : "Tomar"
+}
---
+++ 02_basics/04_objects.js
@@ -0,0 +1,28 @@
+
+// const tinderUsaer = new Object();
+
+const tinderUser ={};
+
+tinderUser.id = "123"
+tinderUser.name = "Say";
+
+
+const reg = {
+    email: "Soga@gail.cion",
+    fullnae : {
+        userFullname : {
+            firstname :"Vsihal",
+            lastname : "To"
+        }
+    }
+}
+
+// console.log(reg.fullnae);
+// console.log(reg.fullnae.userFullname);
+// console.log(reg.fullnae.userFullname.firstname);
+
+
+const obj1 = {
+    1 : "a" ,
+    2 : "b"
+}
