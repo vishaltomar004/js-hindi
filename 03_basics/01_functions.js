d711e19d9915f283dac9043b640714337acae9f8
Vishal Tomar <90099169+vishaltomar004@users.noreply.github.com>
2025-02-19 19:19:44 +0000

    functions completed with some understanding of object

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,25 @@
+function logger(username)
+{
+    return `${username} just logged in`;
+}
+console.log(logger("vivek"));
+
+//  nfunction with infinite parameter
+
+function calculateCartPrice(num1)
+{
+    return num1;
+}
+
+const user = {
+    username : "Vishal",
+    price : 99
+}
+
+function handleObject(anyObject) {
+console.log(`User name is ${anyObject.username} ` );
+
+}
+
+handleObject(user)
+
