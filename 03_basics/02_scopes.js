b88a1971f640048d1e2ef87483426ab19ce0b438
Vishal Tomar <90099169+vishaltomar004@users.noreply.github.com>
2025-02-19 21:03:31 +0000

    Functions hoisting completed . but not with logic only with theor.

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,55 @@
+// // let a = 10;
+// // const b=20;
+// // var c =30;
+// let a =20;
+// if(true)
+// {
+//      a = 10;
+//     // const b=20;
+//     var c =30;
+// }
+
+// console.log(a);
+// // console.log(b);
+// console.log(c
+// );
+
+
+// function a()
+// {
+//     const username ="Vsihal"
+//      function b()
+//      {
+//         const website = "youtube";
+//         console.log("Inner" , username);
+        
+//      }
+//     //  console.log(website);
+
+//      b();
+     
+// }
+// a();
+
+if(true)
+{
+    const username ="Bivek"
+
+    if(username === "Bivek")
+    {
+        const website = "youtube"
+        console.log(username+ website);
+        
+    }
+    // console.log(website);
+    
+}
+
+// console.log(username);
+
+
+function addOne(num)
+{
+    return num +1;
+}
+ 
