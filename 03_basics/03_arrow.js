1e9269d24c01424a49650d17d317706ddb71234d
Vishal Tomar <90099169+vishaltomar004@users.noreply.github.com>
2025-02-20 07:49:01 +0000

    Arrow function

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,42 @@
+// const user = {
+//     username : "hitesh",
+//     price :90,
+
+//     welcomeMessege : function()
+//     {
+//       console.log(`${this.username} , welcome user`);
+//      console.log(this);
+      
+//     }
+// }       
+
+// user.welcomeMessege();
+// // console.log(user.username)
+// // user.username = "Prem";
+// // user.welcomeMessege();
+// // console.log(user.username);
+// // console.log(this);
+
+
+// function chao()
+// {
+//     console.log(this);
+    
+// }
+// chao()
+
+// const chai = () => 
+// {
+
+// }
+
+// const addTwo = (num1 , num2) =>
+// {
+//     return num1 + num2;
+// }
+
+// console.log(addTwo(3,4));
+const addTwo = (num1 , num2) => (num1 + num2);
+    
+    
+    console.log(addTwo(3,4));
