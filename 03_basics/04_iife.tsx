386dbd9342261e8b999b971fe3a2c34f757d52e9
Vishal Tomar <90099169+vishaltomar004@users.noreply.github.com>
2025-02-20 07:59:24 +0000

    iife done in life

---
+++ 03_basics/04_iife.tsx
@@ -0,0 +1,20 @@
+// // // function chai()
+// // // {
+// // //     console.log("COnnection");
+    
+// // // }
+// // // chai();
+
+// // (function chai ()
+// // {
+// //     console.log("DB connections");
+    
+// // })();
+
+// // () -> ()
+// First paranthesis is declaration and 2nd is execution
+
+( (name) => {
+    console.log(`Chai bha iyo naam ha thara ${name}`);
+    
+})("Nom hai bhai");
