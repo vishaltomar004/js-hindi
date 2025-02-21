6fae38fe2d1ba88e11743ec056b4a8a547b0f763
Vishal Tomar <90099169+vishaltomar004@users.noreply.github.com>
2025-02-21 13:00:01 +0000

    control flow  , i Knew them so didi not wrote much

---
+++ 04_controlflow/one.js
@@ -0,0 +1,11 @@
+if(true)
+{
+
+}
+
+// shorthand
+
+const balance =100;
+
+balance ==100 ? console.log("Yess") : console.log("NO");
+;
---
+++ 04_controlflow/truty.js
@@ -0,0 +1,18 @@
+const userEmail = "vja@hama";
+if(userEmail)
+    {
+        console.log("got usetr email");
+}
+else 
+{
+    console.log("no email");
+    
+}
+
+const a = [1];
+
+if(a)
+    
+    {
+        console.log(p);
+    }
