2e2c27789fe5b620ba7f4c9e2f94ce6342b47b05
Vishal Tomar <90099169+vishaltomar004@users.noreply.github.com>
2025-03-24 19:01:34 +0000

    Another method to do the  same

--- 07_projects/Color Changer/colorChangerScript.js
+++ 07_projects/Color Changer/colorChangerScript.js
@@ -1,6 +1,39 @@
  const buttons = document.querySelectorAll('.button');
  const body = document.querySelector("body");
 
+//  let grey = document.getElementById("grey");
+//  grey.addEventListener('click' , function(e)
+// {
+//   console.log("grey Clicked");
+  
+//   body.style.backgroundColor = "grey";
+// })
+// let white = document.getElementById("white");
+//  white.addEventListener('click' , function(e)
+// {
+//   console.log("White Clicked");
+  
+//   body.style.backgroundColor = "white";
+// })
+// let yellow = document.getElementById("yellow");
+//  yellow.addEventListener('click' , function(e)
+// {
+//   console.log("yellow Clicked");
+  
+//   body.style.backgroundColor = "yellow";
+// })
+
+// let blue = document.getElementById("blue");
+//  blue.addEventListener('click' , function(e)
+// {
+//   console.log("blue Clicked");
+  
+//   body.style.backgroundColor = "blue";
+// })
+
+
+
+// Method 2 
  buttons.forEach( function(button) 
 {
     // console.log(button);
